a76672205fe03aca992da58cb45a7c2bfa7fa9fd
jukafah <steve.momcilovic@gmail.com>
2017-10-15 19:35:01 -0500

    revert host

--- wdio.cucumber.conf.js
+++ wdio.cucumber.conf.js
@@ -3,8 +3,8 @@ exports.config = {
         /**
          * server configurations
          */
-        // host: '0.0.0.0',
-        host: '127.0.0.1',
+        host: '0.0.0.0',
+        // host: '127.0.0.1',
         port: 4444,
         path: '/wd/hub',
     
